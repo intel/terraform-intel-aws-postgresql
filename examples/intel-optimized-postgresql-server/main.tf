d4c5621f0eb08ddac3f718f786d89dfa4b6b313f
Rajiv Mandal <rajiv.mandal@intel.com>
2022-11-16 21:39:28 -0800

    reverting to anonymous vpc-id in example main.tf

--- examples/intel-optimized-postgresql-server/main.tf
+++ examples/intel-optimized-postgresql-server/main.tf
@@ -26,5 +26,5 @@ module "optimized-postgresql-server" {
   # Update the vpc_id below for the VPC that this module will use. Find the vpc-id in your AWS account
   # from the AWS console or using CLI commands. In your AWS account, the vpc-id is represented as "vpc-",
   # followed by a set of alphanumeric characters. One sample representation of a vpc-id is vpc-0a6734z932p20c2m4
-  vpc_id = "vpc-5ea60f23"
+  vpc_id = "vpc-0a6734z932p20c2m4"
 }
